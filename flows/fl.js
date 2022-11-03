c5da71658337ecd14e6f0f0011cc960607ed1b1b
Ihar Skavysh <igogoblin@gmail.com>
2022-11-03 14:40:25 +0300

    feat: add simple routing

--- flows/fl.js
+++ flows/fl.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { constants } from "fs";
 import { open, read, close,createReadStream, createWriteStream,readFile } from 'fs';
 import { createGzip } from 'zlib';
+import http from 'http';
 
 // // read all file
 let text = fs.readFileSync('readme.txt','utf-8');
@@ -33,4 +34,73 @@ let writeableStream = fs.createWriteStream('readme.txt');
 
 //  readableStream.pipe(writeableStream);
 
-readableStream.pipe(createGzip()).pipe(writeableStream);
+// архивируем файл
+// readableStream.pipe(createGzip()).pipe(writeableStream);
+// запуск сервера  для остановки ctr + C ------------------------------
+// let time = new Date();
+// http.createServer((request, response)=>{
+// // response.statusCode = 200;
+// response.setHeader('Content-Type', 'text/html');
+// response.statusCode = 200;
+
+// response.write(`<b>${time}</b>`);
+// 	response.end();
+// }).listen(3000);
+
+// http.createServer((request, response) => {
+// 	response.writeHead(200, {'Content-Type': 'text/html'});
+// 	response.end();
+// }).listen(3000);
+
+
+// подсчет запросов к серверу -----------------------------------------
+// let i = 0;
+// http.createServer((request, response) => {
+// 	response.setHeader('Content-Type', 'text/html');
+// 	response.statusCode = 200;
+// 	response.write(String(++i));
+//   if(i==3){
+//     response.write('<p>wrode tak</p>');
+   
+//   }
+// 	 response.end();
+// }).listen(3000);
+
+// http.createServer((request, response) => {
+// 	console.log(request.url.host);
+// 	console.log(request.method);
+// 	console.log(request.headers);
+	
+//   // console.log(response.text);
+
+// 	response.writeHead(200, {'Content-Type': 'text/html'});
+//   response.write('text');
+// 	// response.write(`<p>${GET}`);
+
+// 	response.end();
+// }).listen(3000);
+
+
+http.createServer((request, response) => {
+	if (request.url != '/favicon.ico') {
+		let text;
+		let status = 200;
+		
+		if (request.url == '/page1') {
+			text = '1';
+		}
+		else if (request.url == '/page2') {
+			text = '2';
+		}
+		else if (request.url == '/page3') {
+			text = '3';
+		} else {
+			text = 'page not found';
+			status = 404;
+		}
+		
+		response.writeHead(status, {'Content-Type': 'text/html'});
+		response.write(text);
+		response.end();
+	}
+}).listen(3000);
